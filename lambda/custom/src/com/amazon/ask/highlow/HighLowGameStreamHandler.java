e407c6eddf58eccf9ef908a8955350157687b082
olivia doran <olivia.m.doran@vanderbilt.edu>
2018-11-11 11:28:32 -0600

    working on deployment

--- lambda/custom/src/com/amazon/ask/highlow/HighLowGameStreamHandler.java
+++ lambda/custom/src/com/amazon/ask/highlow/HighLowGameStreamHandler.java
@@ -30,8 +30,7 @@ public class HighLowGameStreamHandler extends SkillStreamHandler {
                 .addExceptionHandler(new ExceptionHandler())
                 .withTableName("HighLowGame")
                 .withAutoCreateTable(true)
-                // Add your skill id below
-                //.withSkillId("")
+                .withSkillId("amzn1.ask.skill.bf8bd6c3-a165-4f0c-9e78-f649ef494ca1")
                 .build();
     }
 
