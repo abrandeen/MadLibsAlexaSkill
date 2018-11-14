175b2cc635381d6ab69513fc3daa574d03f0ae17
olivia doran <olivia.m.doran@vanderbilt.edu>
2018-11-14 14:56:16 -0600

    simplified NoIntentHandler and YesIntentHandler

--- lambda/custom/src/main/java/com/amazon/ask/highlow/handlers/YesIntentHandler.java
+++ lambda/custom/src/main/java/com/amazon/ask/highlow/handlers/YesIntentHandler.java
@@ -3,6 +3,7 @@ package com.amazon.ask.highlow.handlers;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.highlow.MadLib;
+import com.amazon.ask.highlow.WordTypeIntentHelper;
 import com.amazon.ask.model.Response;
 
 import java.util.Map;
@@ -13,18 +14,9 @@ import static com.amazon.ask.request.Predicates.intentName;
 public class YesIntentHandler implements RequestHandler {
     @Override
     public boolean canHandle(HandlerInput input) {
-        // only start a new game if yes is said when not playing a game.
-        boolean isCurrentlyPlaying = false;
-        Map<String, Object> sessionAttributes = input.getAttributesManager().getSessionAttributes();
-
-        /* if game is currently playing then the game state should not be null (check to avoid nullptr)
-           and the game state should also be 'started' */
-        if (sessionAttributes.get("gameState") != null && sessionAttributes.get("gameState").equals("STARTED")) {
-            isCurrentlyPlaying = true;
-        }
 
         // check if not already playing and the user's input matches the YesIntent
-        return !isCurrentlyPlaying && input.matches(intentName("AMAZON.YesIntent"));
+        return !WordTypeIntentHelper.isCurrentlyPlaying(input) && input.matches(intentName("AMAZON.YesIntent"));
     }
 
     @Override
