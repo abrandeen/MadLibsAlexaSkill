8ad5daad3e22ee4003b90de475717e8b5c94edb7
olivia doran <olivia.m.doran@vanderbilt.edu>
2018-11-27 09:17:56 -0600

    changing response strings to match mad lib game

--- lambda/custom/src/main/java/com/amazon/ask/highlow/Constants.java
+++ lambda/custom/src/main/java/com/amazon/ask/highlow/Constants.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Constants {
-    public static final String FALLBACK_MESSAGE_DURING_GAME = "The High Low Game skill can't help you with that.  Try guessing a number between 0 and 100.";
-    public static final String FALLBACK_REPROMPT_DURING_GAME = "Please guess a number between 0 and 100.";
-    public static final String FALLBACK_MESSAGE_OUTSIDE_GAME = "The High Low Game skill can't help you with that.  It will come up with a number between 0 and 100 and you try to guess it by saying a number in that range. Would you like to play?";
+    public static final String FALLBACK_MESSAGE_DURING_GAME = "Please give the requested word type.";
+    public static final String FALLBACK_REPROMPT_DURING_GAME = "Please give the requested word type.";
+    public static final String FALLBACK_MESSAGE_OUTSIDE_GAME = "Please give the requested word type. Would you like to play?";
     public static final String FALLBACK_REPROMPT_OUTSIDE_GAME = "Say yes to start the game or no to quit.";
     public static final String EXCEPTION_MESSAGE = "Sorry, I can't understand the command. Please say again.";
 
